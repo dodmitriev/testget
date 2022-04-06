25c53d369694d94a25259cdbd4208703dda70777
Дмитриев Дмитрий Олегович <indiana@narp.local>
2022-04-06 15:40:44 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# это адрес по которому DJANGO отработает get
+URL = 'http://a91391d8.beget.tech/get/'
+
+client = requests.session()
+
+condition={'search': 'открыть форму'}
+r = client.get(URL, params=condition)
+
+print(r.url)
+print(r.text) # это возвращает сервер - return JsonResponse...
+
+
+
